629d7e06ed6660a960899a6fa09e5e71af032b45
Ainur935 <ainur935@mail.ru>
2021-11-28 19:27:51 +0300

    beginn

---
+++ NewsPaper/news/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_user = models.IntegerField(default=0)
+
+    def update_rating(self, new_rating_user):
+        self.rating_user = new_rating_user
+        self.save()
+
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    is_this_news = models.BooleanField(default=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    heading = models.CharField(max_length=255)
+    text = models.TextField()
+    post_rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.post_rating += 1
+        self.save()
+
+    def dislike(self):
+        self.post_rating -= 1
+        self.save()
+
+    def preview(self):
+        len_ = 124 if len(self.text) > 124 else len(self.text)
+        return self.text[:len_]+'...'
+
+
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    comment_datetime = models.DateTimeField(auto_now_add=True)
+    comment_rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.comment_rating += 1
+        self.save()
+
+    def dislike(self):
+        self.comment_rating -= 1
+        self.save()
+
+
+
